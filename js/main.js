21fa49ecd8acf0920d246284a76cf320eaf7b1f0
luciobo <luciobo@icloud.com>
2022-11-15 14:58:46 +0100

    creato array di oggetti

--- js/main.js
+++ js/main.js
@@ -1,2 +1,48 @@
 // devo iniziare crenado un'array di oggetti
 
+const ilNostroTeam = [
+    {
+        nome: "Wayne",
+        cognome: "Barnett",
+        ruoloAziendale: "Founder & CEO",
+        foto: "wayne-barnett-founder-ceo.jpg",
+    }, {
+        nome: "Angela",
+        cognome: "Caroll",
+        ruoloAziendale: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg",
+    }, {
+        nome: "Walter",
+        cognome: "Gordon",
+        ruoloAziendale: "Office Manager",
+        foto: "walter-gordon-office-manager.jpg",
+    }, {
+        nome: "Angela",
+        cognome: "Lopez",
+        ruoloAziendale: "Social Media Manager",
+        foto: "angela-lopez-social-media-manager.jpg",
+    }, {
+        nome: "Scott",
+        cognome: "Estrada",
+        ruoloAziendale: "Developer",
+        foto: "scott-estrada-developer.jpg",
+    }, {
+        nome: "Barbara",
+        cognome: "Ramos",
+        ruoloAziendale: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg",
+    }
+]
+
+
+
+
+
+
+
+// Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	            scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	    barbara-ramos-graphic-designer.jpg
